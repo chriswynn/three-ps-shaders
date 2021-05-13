e97bf30d2b69419c79d61d6648a681ee2e14309f
chriswynn <chris@wynncodes.com>
2021-05-12 22:06:56 -0400

    add app container styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "minireset.css";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
-
+const MyApp = ({ Component, pageProps }) => (
+  <div
+    style={{
+      backgroundColor: "#cbcbcb",
+      minHeight: "100vh",
+      width: "100%",
+    }}
+  >
+    <Component {...pageProps} />
+  </div>
+);
 export default MyApp;
