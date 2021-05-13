d32e8e3bad01f8b4fcb38e539d28baee859536d5
chriswynn <chris@wynncodes.com>
2021-05-12 22:30:14 -0400

    add system font stack

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ const MyApp = ({ Component, pageProps }) => (
   <div
     style={{
       backgroundColor: "#cbcbcb",
+      fontFamily:
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
       minHeight: "100vh",
       width: "100%",
     }}
